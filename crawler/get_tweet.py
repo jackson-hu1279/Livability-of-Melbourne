1cb6b21bc378fc696caeb27e3e5971afb2f9f4ae
kkiill <250616872@qq.com>
2022-04-24 11:18:25 +0800

    delete unecessary code

---
+++ crawler/get_tweet.py
@@ -0,0 +1,9 @@
+import tweepy
+
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAALWBbQEAAAAA%2FbQ0tpIE3uy14yUmYU0AiocoH6c%3DDkX3Fl2TdMFgRBCivYCSMajfqglkm8DkyylcAXkUFFceAIOBRB"
+
+import tweepy
+
+client = tweepy.Client(bearer_token= BEARER_TOKEN)
+
+query = 'from:suhemparack -is:retweet'
