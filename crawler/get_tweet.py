a8a9137ace173976ef0949d6f5973bbdd8e9a195
kkiill <250616872@qq.com>
2022-04-28 14:17:11 +0800

    some guide about the code

--- crawler/get_tweet.py
+++ crawler/get_tweet.py
@@ -3,7 +3,6 @@ import pickle
 import time
 
 
-
 def reform_tweet(raw_tweet):
     """
     Reform the tweet data into a dictionary form.
@@ -24,15 +23,14 @@ def preprocess(raw_tweet, id_set, tweet_list):
     Process all tweets in one turn, remove tweets withou geo info
     """
     for data in raw_tweet:
-        if(data.geo != None): # Only keep tweets with geo information
-            if(data.id not in id_set):  #remove duplicate
-                id_set.add(data.id)
-                tweet_list.append(reform_tweet(data))
+      if(data.id not in id_set):  #remove duplicate
+        id_set.add(data.id)
+        tweet_list.append(reform_tweet(data))
     
     return tweet_list, id_set
 
 
-def get_tweet_1(query, token):
+def get_tweet_1(query, token, tweets_each_turn, next_page = None):
     """
     Get tweet using Twitter API 2.0. 
     Using keywords(query) as a main parameter
@@ -40,12 +38,14 @@ def get_tweet_1(query, token):
     """
     client = tweepy.Client(bearer_token= token)
     # each time capture 100 tweets
-    tweets = client.search_recent_tweets(query=query, tweet_fields=['author_id', 'created_at', 'geo'], max_results=100)
+    tweets = client.search_recent_tweets(query=query, tweet_fields=['author_id', 'created_at', 'geo'], max_results=tweets_each_turn, next_token=next_page)
 
-    return tweets.data
+    print(tweets.meta)
+
+    return tweets.data, tweets.meta['next_token']
 
 
-def crawler(query, tokens, tweets_each_turn, turns):
+def crawler(query, tokens, tweets_each_turn, turns, file_name):
     """
     Main function. Use query as filter, token for authocation, and number of tweets each turn
     Turns suggest how many turns this function will run
@@ -59,22 +59,28 @@ def crawler(query, tokens, tweets_each_turn, turns):
 
     time_gap = int (tweets_each_turn / len(tokens)) + 1
     count = 0
+    next_page = None
     tweet_list = []
     id_set = set()
     while(count < turns):
         for token in tokens:
-            data = get_tweet_1(query, token)
+            data, next_page = get_tweet_1(query, token, tweets_each_turn, next_page)
             tweet_list, id_set = preprocess(data, id_set, tweet_list)
             time.sleep(time_gap)
         
         count = count + 1
+
+    output = open(file_name,'wb')
+    pickle.dump(tweet_list, output)
+    output.close()
     
-    return tweet_list
-        
+    return
 
 
 #Set up
 BEARER_TOKEN = ["AAAAAAAAAAAAAAAAAAAAALWBbQEAAAAA%2FbQ0tpIE3uy14yUmYU0AiocoH6c%3DDkX3Fl2TdMFgRBCivYCSMajfqglkm8DkyylcAXkUFFceAIOBRB",
-"AAAAAAAAAAAAAAAAAAAAAF1YbQEAAAAAEOLr26RmQ1V0eVq1xDR%2FUioYOKY%3DAHtIcXsDHv5lnyzj8KAdzlEbVVaC85k3uvvUvYESyeK0h9knqM"]
+"AAAAAAAAAAAAAAAAAAAAAF1YbQEAAAAAEOLr26RmQ1V0eVq1xDR%2FUioYOKY%3DAHtIcXsDHv5lnyzj8KAdzlEbVVaC85k3uvvUvYESyeK0h9knqM"]                
+query1 = '#Melbourne lang:en'
 
-query = 'from:suhemparack -is:retweet'
+
+crawler(query1, BEARER_TOKEN, 100, 1, 'melbourne.pkl')
---
+++ crawler/readme.txt
@@ -0,0 +1,5 @@
+Use Section Run in test.ipynb
+Or use get_tweet.py
+Do NOT use other files.
+
+docker files are copied from previous project. Not very sure about its function.
