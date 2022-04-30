c6d906ea46721b451ad9cd3610a5645ef9b05267
SiwatChairat <siwat.chairat@gmail.com>
2022-04-30 17:54:47 +1000

    KeplerGL initialised

--- frontend/webapp/src/App.js
+++ frontend/webapp/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider, useDispatch } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { taskMiddleware } from "react-palm/tasks";
+
+import KeplerGl from "kepler.gl";
+import { addDataToMap } from "kepler.gl/actions";
+import keplerGlReducer from "kepler.gl/reducers";
+
+import useSwr from "swr";
+
+const store = configureStore({
+  reducer: {
+    keplerGl: keplerGlReducer,
+  },
+  middleware: [taskMiddleware],
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Map />
+    </Provider>
+  );
+}
+
+function Map() {
+  return (
+    <KeplerGl
+      id="covid"
+      mapboxApiAccessToken={
+        "pk.eyJ1Ijoic2l3YXRjaGFpcmF0IiwiYSI6ImNsMmxjZW5yZTB6engzaW53dmc5Y3FlZzMifQ.NanT0HoZXtIwWkgWhRXp_A"
+      }
+      width={window.innerWidth}
+      height={window.innerHeight}
+    />
   );
 }
 
