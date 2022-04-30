312d831e76e39e54f1c7d03e5d96c1e9536c643c
jackson-hu1279 <renweih@student.unimelb.edu.au>
2022-04-30 16:36:50 +0800

    Store Aurin crime data to CouchDB

---
+++ aurin/crime/store.py
@@ -0,0 +1,18 @@
+import json
+import couchdb
+
+server = couchdb.Server("http://172.26.134.126:5984")
+server.resource.credentials = ("admin", "admin")
+
+with open("results-crime.json") as f:
+    data = json.load(f)
+    records = data["greater_melbourne"]
+
+    # create db if necessary
+    db_name = "aurin-crime"
+    if db_name not in server:
+        server.create(db_name)
+    db = server[db_name]
+
+    for record in records:
+        db.save(record)
