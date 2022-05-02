fa605e9e96d0b4a2eb9cf01fb9e895a912356125
SiwatChairat <siwat.chairat@gmail.com>
2022-05-02 12:41:08 +1000

    aurin kepler heatmap working locally

--- frontend/webapp/src/App.js
+++ frontend/webapp/src/App.js
@@ -1,40 +1,8 @@
 import React from "react";
-import { Provider, useDispatch } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import { taskMiddleware } from "react-palm/tasks";
-
-import KeplerGl from "kepler.gl";
-import { addDataToMap } from "kepler.gl/actions";
-import keplerGlReducer from "kepler.gl/reducers";
-
-import useSwr from "swr";
-
-const store = configureStore({
-  reducer: {
-    keplerGl: keplerGlReducer,
-  },
-  middleware: [taskMiddleware],
-});
+import KeplerMap from "./components/KeplerMapMain";
 
 function App() {
-  return (
-    <Provider store={store}>
-      <Map />
-    </Provider>
-  );
-}
-
-function Map() {
-  return (
-    <KeplerGl
-      id="covid"
-      mapboxApiAccessToken={
-        "pk.eyJ1Ijoic2l3YXRjaGFpcmF0IiwiYSI6ImNsMmxjZW5yZTB6engzaW53dmc5Y3FlZzMifQ.NanT0HoZXtIwWkgWhRXp_A"
-      }
-      width={window.innerWidth}
-      height={window.innerHeight}
-    />
-  );
+  return <KeplerMap />;
 }
 
 export default App;
