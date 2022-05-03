93ce7a9c74b4e945600b474306daee8e6f5b08f4
chiyinwong <chiyinwong97@gmail.com>
2022-05-03 13:05:44 +1000

    added health file lgas

--- adding-LGA/add-LGA.py
+++ adding-LGA/add-LGA.py
@@ -4,7 +4,6 @@ from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 from sqlalchemy import null
 
-
 def write_tweets(filename, tweets):
     with open(filename, "w", encoding="utf-8") as updated_file:
         print('{"docs": [', file=updated_file)
@@ -25,8 +24,7 @@ def write_tweets(filename, tweets):
 with open("vic-lga.geojson", "r") as f:
     gj = geojson.load(f)
 
-# aside:  access the third row (second object) in the geojson file
-# features = gj["features"][2]
+
 
 # Greater melbourne LGAs taken from wikipedia page
 GREATER_MELBOURNE_LGA_LIST = [
@@ -40,6 +38,7 @@ GREATER_MELBOURNE_LGA_LIST = [
     "Whittlesea City", "Wyndham City", "Yarra Ranges Shire"
     ]
 
+# Greater melbourne LGA codes in alphabetical order (Banyule City -> Yarra Ranges Shire)
 GREATER_MELBOURNE_LGA_CODES = [
     20660, 20910, 21110, 21180, 21450, 21610, 21890, 22170, 22310,
     22670, 23110, 23270, 23430, 23670, 24210, 24330, 24410, 24600,
@@ -58,18 +57,18 @@ for i in range(0,92):
         count += 1
         LGA_FEATURES_LIST.append(lga_features)
 
-
+# Convert all the coordinates in the LGA to Polygon type 
+for i in range(len(LGA_FEATURES_LIST)):
+    LGA_FEATURES_LIST[i]["geometry"]["coordinates"][0] = Polygon(LGA_FEATURES_LIST[i]["geometry"]["coordinates"][0])
 
 # print(LGA_FEATURES_LIST)
 # checking that there are 31 features 
-print(len(LGA_FEATURES_LIST))
+# print(len(LGA_FEATURES_LIST))
 # print(LGA_FEATURES_LIST[0]["geometry"])
 # print(LGA_FEATURES_LIST[0]["geometry"]["coordinates"][0])
 # print(type(Polygon(LGA_FEATURES_LIST[0]["geometry"]["coordinates"][0])))
 
-# Convert all the coordinates in the LGA to Polygon type 
-for i in range(len(LGA_FEATURES_LIST)):
-    LGA_FEATURES_LIST[i]["geometry"]["coordinates"][0] = Polygon(LGA_FEATURES_LIST[i]["geometry"]["coordinates"][0])
+
 
 # print(LGA_FEATURES_LIST[30]["geometry"]["coordinates"][0])
 
@@ -86,6 +85,34 @@ for i in range(len(LGA_FEATURES_LIST)):
 
 # here for our new version of the code, we would be opening crimes.json and health.json
 # crimes.json
+
+# def attach_lga(filename):
+#     with_lga = []
+#     without_lga = []
+
+#     with open ("crimes.json", "r", encoding = "utf-8") as tweets_json:
+#         tweet_data = json.load(tweets_json)
+#         for tweet in tweet_data['docs']:
+#             coords = tweet.get('coordinates')
+#             if coords:
+#                 point = Point(coords["coordinates"])  
+#                 for j in range(len(LGA_FEATURES_LIST)):
+#                     if LGA_FEATURES_LIST[j]["geometry"]["coordinates"][0].contains(point): # if true, add the LGA name and code to the tweet as a new key
+#                         tweet["LGA_NAME"] = LGA_FEATURES_LIST[j]["properties"]["LGA_NAME"]
+#                         tweet["LGA_CODE"] = LGA_FEATURES_LIST[j]["properties"]["LGA_CODE"]
+#                         print("in LGA", LGA_FEATURES_LIST[j]["properties"]["LGA_NAME"], LGA_FEATURES_LIST[j]["properties"]["LGA_CODE"])
+#                         print(tweet["LGA_NAME"])
+#                         with_lga.append(tweet)
+            
+#             if "LGA_NAME" not in tweet:
+#                 print("not in LGA")
+#                 tweet["LGA_NAME"] = "NO LGA"
+#                 tweet["LGA_CODE"] = "NO LGA CODE"
+#                 without_lga.append(tweet)
+    
+#     return(tweet_data, with_lga, without_lga)
+
+# Crimes
 
 with_lga = []
 without_lga = []
@@ -110,12 +137,38 @@ with open ("crimes.json", "r", encoding = "utf-8") as tweets_json:
             tweet["LGA_CODE"] = "NO LGA CODE"
             without_lga.append(tweet)
 
-# Step 3 - write to new json file
-
     write_tweets('crimes-updated.json', tweet_data['docs'])
     write_tweets('crimes-updated-lga.json', with_lga)
     write_tweets('crimes-updated-without-lga.json', without_lga)
 
+# Health 
+
+with_lga = []
+without_lga = []
+
+with open ("health.json", "r", encoding = "utf-8") as tweets_json:
+    tweet_data = json.load(tweets_json)
+    for tweet in tweet_data['docs']:
+        coords = tweet.get('coordinates')
+        if coords:
+            point = Point(coords["coordinates"])  
+            for j in range(len(LGA_FEATURES_LIST)):
+                if LGA_FEATURES_LIST[j]["geometry"]["coordinates"][0].contains(point): # if true, add the LGA name and code to the tweet as a new key
+                    tweet["LGA_NAME"] = LGA_FEATURES_LIST[j]["properties"]["LGA_NAME"]
+                    tweet["LGA_CODE"] = LGA_FEATURES_LIST[j]["properties"]["LGA_CODE"]
+                    print("in LGA", LGA_FEATURES_LIST[j]["properties"]["LGA_NAME"], LGA_FEATURES_LIST[j]["properties"]["LGA_CODE"])
+                    print(tweet["LGA_NAME"])
+                    with_lga.append(tweet)
+        
+        if "LGA_NAME" not in tweet:
+            print("not in LGA")
+            tweet["LGA_NAME"] = "NO LGA"
+            tweet["LGA_CODE"] = "NO LGA CODE"
+            without_lga.append(tweet)
+
+    write_tweets('health-updated.json', tweet_data['docs'])
+    write_tweets('health-updated-lga.json', with_lga)
+    write_tweets('health-updated-without-lga.json', without_lga)
     
 
 # while file_num < total_files:
@@ -163,31 +216,3 @@ with open ("crimes.json", "r", encoding = "utf-8") as tweets_json:
 #                 updated_file.write(",")
 #                 updated_file.write('\n')
 #         print(']}', file=updated_file)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
